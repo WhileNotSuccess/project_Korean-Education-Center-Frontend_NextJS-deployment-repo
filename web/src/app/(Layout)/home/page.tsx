e85d647bdf6ad455520d27434c4f0a2b76d7a9c1
MunSeongyun <borygashill608@gmail.com>
2025-02-13 19:51:16 +0900

    merge feat/login

--- web/src/app/(Layout)/home/page.tsx
+++ web/src/app/(Layout)/home/page.tsx
@@ -1,10 +1,13 @@
+import HomePageCompo from "../components/HomePageCompo";
 import SetCookie from "../../common/Cookie";
+import CheckNewUser from "@/app/(NoLayout)/components/CheckNewUser";
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
       <SetCookie />
-      <div>home 페이지입니다.</div>
+      <CheckNewUser />
+      <HomePageCompo />
     </div>
   );
 }
