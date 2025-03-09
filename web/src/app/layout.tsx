f05bb2a3eb2b548b3de380b08d44c5ee98b19f8a
kwongihyeon <rrg1591rrg@gmail.com>
2025-03-09 16:04:11 +0900

    오픈그래프 설정 변경

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -19,9 +19,17 @@ export const metadata: Metadata = {
     icon:"/images/logo.svg"
   },
   openGraph:{
+    url : "https://kcenter.yju.ac.kr",
+    type : "website",
     title:"한국어교육센터 - 영진전문대",
     description:"한국어교육센터는 외국인 대상 한국어 교육 전문 기관으로, 대한민국 교육부로부터 국제화역량 인증대학으로 선정되었습니다. 한국어교육센터터 정규과정을 운영 중입니다.",
-    images:"https://kcenter.yju.ac.kr/images/yeungjinLogoOpenGraph.png"
+    images:[
+      {
+        url:"https://kcenter.yju.ac.kr/images/yeungjinLogoOpenGraph.png",
+        width : "1200",
+        height : "630"
+      }
+    ]
   }
 };
 
